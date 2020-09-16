590ab2af490bfc388504e9025bdfaa523f63ac75
Laise Viana <laisevn@gmail.com>
2020-09-16 19:31:53 -0300

    test: Teste for SinUpController implementation

---
+++ spec/presentation/controllers/SignUpControllerSpec.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace spec\presentation\controllers;
+
+use PhpSpec\ObjectBehavior;
+
+class SignUpControllerSpec extends ObjectBehavior
+{
+    function it_returns_error_if_name_not_provided()
+    {
+        $http_request = "body: {
+        'email': 'some-email@example.com', 
+        'password': 1234,
+        'password_confirmation': 1234,
+        }";
+    
+        $response = $this->handle($http_request);
+        $response->getStatusCode()->shouldBe(400);
+    }
+}
