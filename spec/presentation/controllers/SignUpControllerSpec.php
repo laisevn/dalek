ca92f4f360b46aba5ef16191176757a4626b12ad
Laise Viana <laisevn@gmail.com>
2020-09-17 21:07:06 -0300

    feat: ensure SignUpController returns 400 if no cpf if provided

--- spec/presentation/controllers/SignUpControllerSpec.php
+++ spec/presentation/controllers/SignUpControllerSpec.php
@@ -6,16 +6,37 @@ use PhpSpec\ObjectBehavior;
 
 class SignUpControllerSpec extends ObjectBehavior
 {
-    public function it_returns_error_if_name_not_provided()
+    public function it_returns_error_if_name_is_not_provided()
     {
-        $http_request = "body: {
-        'email': 'some-email@example.com',
-        'password': 1234,
-        'password_confirmation': 1234,
-      }";
+
+        $http_request = json_encode((object) [
+            'body' => [
+                'email' => 'some-email@example.com',
+                'cpf' => '98540234009',
+                'password' => 1234,
+                'password_confirmation' => 1234,
+            ],
+        ]);
 
         $response = $this->handle($http_request);
         $response->shouldHaveJsonKeyWithValue('statusCode', 400);
         $response->shouldHaveJsonKeyWithValue('body', 'Missing param: name');
+    }
+
+    public function it_returns_error_if_cpf_is_not_provided()
+    {
+
+        $http_request = json_encode((object) [
+            'body' => [
+                'name' => 'Some Name',
+                'email' => 'some-email@example.com',
+                'password' => 1234,
+                'password_confirmation' => 1234,
+            ],
+        ]);
+
+        $response = $this->handle($http_request);
+        $response->shouldHaveJsonKeyWithValue('statusCode', 400);
+        $response->shouldHaveJsonKeyWithValue('body', 'Missing param: cpf');
     }
 }
--- src/presentation/controllers/SignUpController.php
+++ src/presentation/controllers/SignUpController.php
@@ -4,11 +4,19 @@ namespace presentation\controllers;
 
 class SignUpController
 {
-    public function handle($argument1)
+    public function handle($http_request)
     {
+        $request = json_decode($http_request);
+
+        if (empty($request->{'body'}->{'name'})) {
+            $param = 'name';
+        } elseif (empty($request->{'body'}->{'cpf'})) {
+            $param = 'cpf';
+        }
+
         $data = (object) [
             'statusCode' => 400,
-            'body' => 'Missing param: name',
+            'body' => "Missing param: ${param}",
         ];
         $response = json_encode($data);
         return $response;
