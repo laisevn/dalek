588817b1500c6c6de16b15c53f0ce6a857182ef0
Laise Viana <laisevn@gmail.com>
2020-09-24 10:59:43 -0300

    feat: Add Middlewares to App

--- src/Main/App.php
+++ src/Main/App.php
@@ -6,6 +6,6 @@ require __DIR__ . './../../vendor/autoload.php';
 
 require __DIR__ . './../Main/DotEnv.php';
 require __DIR__ . './../Main/Server.php';
-// require __DIR__ . './../Main/Middlewares.php';
+require __DIR__ . './../Main/Middlewares.php';
 require __DIR__ . './../routes.php';
 // require __DIR__ . './../Main/NotFound.php';
