5d64bbb725abec2f58ef34cf266f2cb4b049d917
Laise Viana <laisevn@gmail.com>
2020-09-23 21:44:24 -0300

    feat: add signup route to api

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,8 @@
 <?php
 /**Routes */
 
+/** Account Routes */
 $app->get('/', 'Presentation\Controllers\HomeController:getHelp');
+
+/** Payment Routes */
+$app->get('/signup', 'Presentation\Controllers\SignUpController');
